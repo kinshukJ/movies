faa02f9b55e67477774cf8b666ad3c46b55a23bc
Kinshuk <kinshukjuneja@gmail.com>
2017-02-06 15:29:38 -0500

    Finished assignment 1

---
+++ Movies/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Movies
+//
+//  Created by Kinshuk Juneja on 2/5/17.
+//  Copyright © 2017 CS490. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
